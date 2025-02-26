3c848aa9e549eaea7046a992a68d690363b9ca52
Eugene Odeluga <eodeluga@users.noreply.github.com>
2025-02-26 05:13:30 +0000

    TASK #201: Updated property to use correct type

--- src/types/middleware.type.ts
+++ src/types/middleware.type.ts
@@ -1,9 +1,9 @@
 import { NextApiResponse } from 'next/types'
 import { INextApiRequest } from '@/types/next.type'
-import { ErrorObj } from '@/types/error-obj.type'
+import { BaseError } from '@/utils/errors.util'
 
 export type Middleware<T = object> = {
   before?: (req: INextApiRequest<T>, res: NextApiResponse) => Promise<void>
   after?: (req: INextApiRequest<T>, res: NextApiResponse, data: unknown) => Promise<void>
-  onError?: (err: ErrorObj, req: INextApiRequest<T>, res: NextApiResponse) => Promise<void>
+  onError?: (err: BaseError, req: INextApiRequest<T>, res: NextApiResponse) => Promise<void>
 }
