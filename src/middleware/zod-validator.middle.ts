7d1f64b1452113ccc598414df2fccafc4a468c69
Eugene Odeluga <eodeluga@users.noreply.github.com>
2025-09-30 00:01:16 +0100

    Task-#285: Broke out error classes

--- src/middleware/zod-validator.middle.ts
+++ src/middleware/zod-validator.middle.ts
@@ -1,24 +1,6 @@
 import { z } from 'zod'
-import type { ZodIssue } from 'zod/v3'
 import type { NextMiddyApiRequest, NextMiddyLifecycle } from '@/utils/next-middy.util'
-import { BaseError } from '@/types/base-error.type'
-
-export class ZodValidationError extends BaseError {
-  declare details: {
-    scope?: string
-    issues: ZodIssue[]
-  }
-  
-  constructor(validationType: 'input' | 'output', error: z.ZodError, scope?: string) {
-    super(400, `ZodValidationError: ${validationType}`, error.message, {
-      ...(scope && { scope }),
-      issues: error.issues.map((issue) => ({
-        path: issue.path,
-        message: issue.message,
-      })),
-    })
-  }
-}
+import { ZodValidationError } from '@/errors/zod-validation.error'
 
 // Middleware function for Zod validation
 export const zodValidatorMiddle = <I extends z.ZodTypeAny, O extends z.ZodTypeAny>(
