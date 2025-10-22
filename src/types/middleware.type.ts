99bce7fb36caaea731169a1ac24043cc375f7cf2
Eugene Odeluga <eodeluga@users.noreply.github.com>
2025-10-22 03:45:17 +0100

    Removed redundant types

--- src/types/middleware.type.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiResponse } from 'next/types'
-import { INextApiRequest } from '@/types/next.type'
-import { BaseError } from '@/utils/errors.util'
-
-export type Middleware<T = object> = {
-  before?: (req: INextApiRequest<T>, res: NextApiResponse) => Promise<void>
-  after?: (req: INextApiRequest<T>, res: NextApiResponse, data: unknown) => Promise<void>
-  onError?: (err: BaseError, req: INextApiRequest<T>, res: NextApiResponse) => Promise<void>
-}
